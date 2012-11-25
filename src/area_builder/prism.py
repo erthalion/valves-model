d89a00ab2294439980d4310f0a9c7b3388a29e19
erthalion <9erthalion6@gmail.com>
2012-11-25 19:44:29 +0000

    Modified valves mask

--- src/area_builder/prism.py
+++ src/area_builder/prism.py
@@ -17,32 +17,32 @@ out_boundary = (0*x + 0*y + z == 61)
 middle = (0*x +0*y + z > 1) & (0*x +0*y + z < 60)
 left = (0*x +0*y + z < 20)
 right = (0*x +0*y + z > 40)
-left_boundary = (0*x +0*y + z == 20)
-right_boundary = (0*x + 0*y + z == 40)
+not_left_boundary = (0*x +0*y + z != 20)
+not_right_boundary = (0*x + 0*y + z != 40)
 
 prism = ((x-x0)*(x-x0) + (y-y0)*(y-y0) + 0*z < r*r)
 second_prism = ((x-x0) + (y-y0) + 0*z < R) & (-(x-x0) + (y-y0) + 0*z < R) & ((x-x0) - (y-y0) + 0*z < R) & (-(x-x0) - (y-y0) + 0*z < R)
-mask = prism & (0*x +0*y + z > 1) & (0*x +0*y + z < 60)
-left_mask = second_prism & (0*x + 0*y + z > 1) & (0*x +0*y + z < 20)
-right_mask = second_prism & (0*x + 0*y + z > 40) & (0*x +0*y + z < 60)
+mask = prism & middle
+left_mask = second_prism & left
+right_mask = second_prism & right
 
-first_valve = prism & (x + 0*y + 0*z < 12) & (-(x-x0) + (y-y0) + 0*z > 0) & (0*x +0*y + z > 1) & (0*x +0*y + z < 60)
-second_valve = prism & (x + 0*y + 0*z > 12) & ((x-x0) + (y-y0) + 0*z > 0) & (0*x +0*y + z > 1) & (0*x +0*y + z < 60)
-third_valve = prism & ((x-x0) + (y-y0) + 0*z < 0) & (-(x-x0) + (y-y0) + 0*z < 0)  & (0*x +0*y + z > 1) & (0*x +0*y + z < 60)
+first_valve = prism & (x + 0*y + 0*z < 12) & (-(x-x0) + (y-y0) + 0*z > 0) & not_left_boundary & not_right_boundary & middle
+second_valve = prism & (x + 0*y + 0*z > 12) & ((x-x0) + (y-y0) + 0*z > 0) & not_left_boundary & not_right_boundary & middle
+third_valve = prism & ((x-x0) + (y-y0) + 0*z < 0) & (-(x-x0) + (y-y0) + 0*z < 0) & not_left_boundary & not_right_boundary & middle
 
-input_mask = second_prism & (0*x + 0*y + z == 1)
-output_mask = second_prism & (0*x + 0*y +z == 61)
+input_mask = second_prism & in_boundary
+output_mask = second_prism & out_boundary
 
 array = np.zeros((nx, ny, nz))
 
 array[mask] = 1
-array[input_mask] = 2
-array[output_mask] = 3
 array[left_mask] = 1
 array[right_mask] = 1
 array[first_valve] = 0
 array[second_valve] = 0
 array[third_valve] = 0
+array[input_mask] = 2
+array[output_mask] = 2
 
 output = open("mask.vtk","w")
 
