2ea2f22f114e5035169ad8e3ef99895223bb84ec
erthalion <9erthalion6@gmail.com>
2012-12-01 19:31:20 +0000

    Renewed area builder

--- src/area_builder/prism.py
+++ src/area_builder/prism.py
@@ -12,26 +12,50 @@ r = 6
 R = 12
 
 x, y, z = np.ogrid[0:nx, 0:ny, 0:nz]
+
 in_boundary = (0*x + 0*y + z == 1)
-out_boundary = (0*x + 0*y + z == 61)
+out_boundary = (0*x + 0*y + z == 60)
 middle = (0*x +0*y + z > 1) & (0*x +0*y + z < 60)
+middle_valve = (0*x +0*y + z > 20) & (0*x +0*y + z < 40)
+valve = (0*x +0*y + z > 25) & (0*x +0*y + z < 30)
 left = (0*x +0*y + z < 20)
 right = (0*x +0*y + z > 40)
-not_left_boundary = (0*x +0*y + z != 20)
-not_right_boundary = (0*x + 0*y + z != 40)
+left_boundary = (0*x +0*y + z == 20)
+right_boundary = (0*x + 0*y + z == 40)
 
-prism = ((x-x0)*(x-x0) + (y-y0)*(y-y0) + 0*z < r*r)
-second_prism = ((x-x0) + (y-y0) + 0*z < R) & (-(x-x0) + (y-y0) + 0*z < R) & ((x-x0) - (y-y0) + 0*z < R) & (-(x-x0) - (y-y0) + 0*z < R)
-mask = prism & middle
-left_mask = second_prism & left
-right_mask = second_prism & right
+inner_cylinder = ((x-x0)*(x-x0) + (y-y0)*(y-y0) + 0*z < r*r)
 
-first_valve = prism & (x + 0*y + 0*z < 12) & (-(x-x0) + (y-y0) + 0*z > 0) & not_left_boundary & not_right_boundary & middle
-second_valve = prism & (x + 0*y + 0*z > 12) & ((x-x0) + (y-y0) + 0*z > 0) & not_left_boundary & not_right_boundary & middle
-third_valve = prism & ((x-x0) + (y-y0) + 0*z < 0) & (-(x-x0) + (y-y0) + 0*z < 0) & not_left_boundary & not_right_boundary & middle
+outer_prism = ((x-x0) + (y-y0) + 0*z <= R)\
+        & (-(x-x0) + (y-y0) + 0*z <= R)\
+        & ((x-x0) - (y-y0) + 0*z <= R)\
+        & (-(x-x0) - (y-y0) + 0*z <= R)
 
-input_mask = second_prism & in_boundary
-output_mask = second_prism & out_boundary
+mask = inner_cylinder & middle
+
+left_mask = outer_prism & left
+
+right_mask = outer_prism & right
+
+first_valve = inner_cylinder\
+        & (x + 0*y + 0*z < 12)\
+        & (-(x-x0) + (y-y0) + 0*z > 0)\
+        & middle_valve
+
+second_valve = inner_cylinder\
+        & (x + 0*y + 0*z > 12)\
+        & ((x-x0) + (y-y0) + 0*z > 0)\
+        & middle_valve
+
+third_valve = inner_cylinder\
+        & ((x-x0) + (y-y0) + 0*z < 0)\
+        & (-(x-x0) + (y-y0) + 0*z < 0)\
+        & middle_valve
+
+input_mask = outer_prism & in_boundary
+input_left_mask = outer_prism & left_boundary
+
+output_mask = outer_prism & out_boundary
+output_left_mask = outer_prism & right_boundary
 
 array = np.zeros((nx, ny, nz))
 
@@ -42,7 +66,9 @@ array[first_valve] = 0
 array[second_valve] = 0
 array[third_valve] = 0
 array[input_mask] = 2
-array[output_mask] = 2
+array[output_mask] = 3
+array[input_left_mask] = 1
+array[output_left_mask] = 1
 
 output = open("mask.vtk","w")
 
