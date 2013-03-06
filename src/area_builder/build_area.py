edce40a604e247d53435b60ec9ca66aac0dd16cb
erthalion <9erthalion6@gmail.com>
2013-03-06 21:28:44 +0700

    Improved style of area builders modules and added cube area

--- src/area_builder/build_area.py
+++ src/area_builder/build_area.py
@@ -3,12 +3,12 @@
 
 import coord
 import mask
-from empty import empty_pressure_mask, empty_u_mask, empty_v_mask, empty_w_mask
+from empty import pressure_mask, u_mask, v_mask, w_mask
 
 if __name__ == '__main__':
     coord.build_area()
     mask.build_area()
-    empty_pressure_mask.build_area()
-    empty_u_mask.build_area()
-    empty_v_mask.build_area()
-    empty_w_mask.build_area()
+    pressure_mask.build_area()
+    u_mask.build_area()
+    v_mask.build_area()
+    w_mask.build_area()
---
+++ src/area_builder/cube/w_mask.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import numpy as np
+import ConfigParser
+
+def build_area():
+    area = ConfigParser.RawConfigParser()
+    area.read('area.config')
+
+    nx = int(area.get('Area', 'Nx'))
+    ny = int(area.get('Area', 'Ny'))
+    nz = int(area.get('Area', 'dNz'))
+
+    x, y, z = np.ogrid[0:nx, 0:ny, 0:nz]
+
+    in_boundary = (x == 0)
+    out_boundary = (x == nx-1)
+
+    mask = (x >= 0) & (x <= nx-1) &\
+            (y >= 1) & (y <= ny-3) &\
+            (z >= 1) & (z <= nz-2)
+
+    inner_cube = (x >= (nx-1)/3) & (x <= (nx-1)*2/3) &\
+            (y >= (ny-1)/3) & (y <= (ny-1)*2/3) &\
+            (z >= (nz-1)/3) & (z <= (nz-1)*2/3)
+
+    array = np.zeros((nx, ny, nz))
+
+    array[mask] = 1
+    array[in_boundary & mask] = 2
+    array[out_boundary & mask] = 3
+    array[inner_cube] = 0
+
+    """ Write mask file
+    """
+    with file('w_area.mask', 'w') as outfile:
+        for slice_2d in array:
+            np.savetxt(outfile, slice_2d, fmt="%i")
