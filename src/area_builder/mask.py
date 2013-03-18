3cdf1712c11264f30006c9fd1ab7821071febfbd
erthalion <9erthalion6@gmail.com>
2013-03-18 21:07:47 +0700

    Final improvement for cylinder area builder

--- src/area_builder/mask.py
+++ src/area_builder/mask.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import ConfigParser
+from math import tan, pi
 
 def build_area():
     area = ConfigParser.RawConfigParser()
@@ -11,16 +12,41 @@ def build_area():
     nx = int(area.get('Area', 'Nx'))
     ny = int(area.get('Area', 'Ny'))
     nz = int(area.get('Area', 'dNz'))
+    R = ny/2 - 2
+    x0 = nx/2
+    y0 = ny/2
+    z0 = nz/2
+    alpha = pi/4
+    big_valve_width = 2
+    big_valve_shift = nx*2/3
 
     x, y, z = np.ogrid[0:nx, 0:ny, 0:nz]
 
-    mask = (x >= 0) & (x < nx) &\
-            (y >= 0) & (y < ny) &\
-            (z >= 0) & (z < nz)
+    in_boundary = (x == 0)
+    out_boundary = (x == nx-1)
+
+    mask = (x >= 1) & (x <= nx-2) &\
+            (y >= 0) & (y <= ny-2) &\
+            (z >= 0) & (z <= ny-2)
+
+    inner_cylinder = ((y-y0)**2 + (z-z0)**2 < (R+1)**2)
+
+    first_conuse = ((x-big_valve_shift) >= -np.sqrt((y-y0)**2 + (z-z0)**2)*tan(alpha)) &\
+            (x-big_valve_shift <= -R/2) & (x-big_valve_shift >= -R)
+
+    second_conuse = ((x-big_valve_shift - big_valve_width) >= -np.sqrt((y-y0)**2 + (z-z0)**2)*tan(alpha)) &\
+            (x-big_valve_shift - big_valve_width <= -R/3) & (x-big_valve_shift - big_valve_width >= -R)
 
     array = np.zeros((nx, ny, nz))
 
-    array[mask] = 1
+    array[inner_cylinder & mask] = 1
+    array[in_boundary & inner_cylinder] = 2
+    array[out_boundary & inner_cylinder] = 3
+
+    """ Create big valve
+    """
+    array[inner_cylinder & first_conuse] = 0
+    array[inner_cylinder & second_conuse] = 1
 
     """ Generate 3d array of coordinates
     """
