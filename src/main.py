01b961e548a4288e4532c9676d3688e702ea2920
erthalion <9erthalion6@gmail.com>
2013-10-19 11:54:22 +0700

    Added build-info

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@
 from numpy.ctypeslib import load_library
 from numpyctypes import c_ndarray
 
-valves_lib = load_library(
-    'libValveModel.Debug.so', '.')       # '.' is the directory of the C++ lib
 
 def main():
-    valves_lib.main()
+    with open('.build-info', 'r') as build_info:
+        lib_name = build_info.read().strip()
+        valves_lib = load_library(lib_name, '.')
+        valves_lib.main()
 
 
 if __name__ == '__main__':
